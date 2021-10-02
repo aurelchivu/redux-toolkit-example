e100096d544b4ebabeb7272c5d5071f8f1cb7c9a
mac <mac@macs-MacBook-Pro.local>
2021-10-02 18:08:32 +0300

    Set number of items to fetch

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Card = ({ title, date, explanation, image }) => {
   return (
-    <div>
-      <h2>{title}</h2>
-      <h4>{date}</h4>
+    <div className='card'>
+      <h1>{title}</h1>
+      <h3>{date}</h3>
       <img src={image} alt='star picture' />
       <p>{explanation}</p>
       <br />
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './Card';
 import { useGetPicturesQuery } from '../redux/services/nasaApi';
 
 const HomePage = () => {
-  const { data, isFetching } = useGetPicturesQuery();
+  const [numPictures, setNumPictures] = useState(10);
+  const { data = [], isFetching } = useGetPicturesQuery(numPictures);
+  const options = ['5', '10', '15', '20', '25'];
 
-  console.log(data);
   return (
     <div>
-      {isFetching ? <h1>Loading...</h1> : data?.map((item) => (
-        <Card
-          key={item.title}
-          title={item.title}
-          date={item.date}
-          explanation={item.explanation}
-          image={item.url}
-        />
-      ))}
+      {isFetching ? (
+        <h2>Loading...</h2>
+      ) : (
+        <>
+          <h1>Pictures to show:</h1>
+          <select
+            className='selectNum'
+            value={numPictures}
+            onChange={(e) => setNumPictures(Number(e.target.value))}
+          >
+            {options.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+          {data?.map((item) => (
+            <Card
+              key={item.title}
+              title={item.title}
+              date={item.date}
+              explanation={item.explanation}
+              image={item.url}
+            />
+          ))}
+        </>
+      )}
     </div>
   );
 };
